00aaf0999298ce5da45506e2e58e41db27e10708
Amanda Aziz <142847924+Amanda-Aziz@users.noreply.github.com>
2026-06-16 15:51:28 -0300

    Update and rename arq1.c to base.c

---
+++ estudos/MATRIZES/base.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ( ) {
+    int mat [3][2],i,j;
+    for(i = 0; i <= 2; i++) {
+        for(j = 0; j <= 1; j++) {
+            mat[i][j] = i + j;
+        }
+    }
+    for(i = 0; i <= 1; i++) {
+        for(j = 0; j <= 2; j++) {
+            printf("%d ",mat[j][i]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
