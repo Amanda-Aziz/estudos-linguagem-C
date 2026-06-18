3e8d7dd22fce519906d9c2445e7f47e760ba0afb
Amanda Aziz <142847924+Amanda-Aziz@users.noreply.github.com>
2026-06-18 20:52:18 -0300

    Update parametros-mudanca-de-valor.c

--- estudos/PONTEIROS/parametros-mudanca-de-valor.c
+++ estudos/PONTEIROS/parametros-mudanca-de-valor.c
@@ -1,3 +1,40 @@
+/******************************************************************************
+ * DESCRIÇÃO:
+ * Sistema de atualização de valores utilizando passagem de parâmetros por
+ * referência, permitindo aplicar incrementos sucessivos a uma variável
+ * informada pelo usuário.
+ *
+ * FUNCIONAMENTO:
+ * 1. Entrada: O usuário informa o valor inicial de X.
+ * 2. Configuração: O usuário define o valor do incremento e a quantidade de
+ *    vezes que ele será aplicado.
+ * 3. Referência: O endereço de memória da variável X é enviado para a função.
+ * 4. Processamento: O programa realiza a soma do incremento ao valor de X
+ *    repetidamente, conforme a quantidade especificada.
+ * 5. Atualização: A variável original é modificada diretamente através do
+ *    ponteiro recebido pela função.
+ * 6. Saída: O valor final de X é exibido após todas as atualizações.
+ *
+ * O QUE CONTÉM NESTE CÓDIGO:
+ * - Ponteiros: Utilização de parâmetros do tipo ponteiro para acessar e
+ *   modificar variáveis externas à função.
+ * - Passagem por Referência: Alteração direta da variável X através de seu
+ *   endereço de memória.
+ * - Operador de Endereço: Uso do operador & para obter o endereço da variável.
+ * - Operador de Desreferenciação: Uso do operador * para acessar e modificar
+ *   o conteúdo armazenado no endereço apontado.
+ * - Estruturas de Repetição: Aplicação de um laço for para realizar múltiplos
+ *   incrementos.
+ * - Operações Aritméticas: Soma sucessiva de um valor de incremento.
+ * - Modularização: Separação da lógica de processamento em uma função
+ *   específica.
+ * - Entrada e Saída de Dados: Utilização de scanf() e printf() para interação
+ *   com o usuário.
+ *
+ * Autor/User: Amanda-Aziz
+ * Data: 18 de Junho de 2026
+ *******************************************************************************/
+
 //Exercícios de passagem de parâmetros por referência
 
 #include <stdio.h>
