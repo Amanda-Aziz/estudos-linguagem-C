8ddf906c535f3e1223403130bdb21900264d58fd
Amanda Aziz <142847924+Amanda-Aziz@users.noreply.github.com>
2026-08-27 10:03:54 -0300

    Define stack structures and functions in pilha.h
    Added stack data structures and function prototypes.

--- EDL-2026-2/pilha/pilha.h
+++ EDL-2026-2/pilha/pilha.h
@@ -1,5 +1,16 @@
-#include <stdio.h>
+#define pilha_h
 
-int main(){
-  return 0;
-}
+typedef struct no {
+	int dado;
+	struct no *prox;
+}No;
+
+typedef struct pilha {
+	No* inicio;
+}Pilha;
+
+No* criar_no(int novo_dado);
+Pilha *iniciar_pilha();
+void empilhar(Pilha* p, int novo_dado);
+void desempilhar(Pilha* p);
+int top(Pilha* p);
